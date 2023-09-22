c43a602cee2bceca75c636e6e53de3f51fb140fa
AlexSan369 <trigramasolar@gmail.com>
2023-09-22 14:44:58 -0300

    js

--- index.js
+++ index.js
@@ -50,3 +50,78 @@ class UI {
         document.querySelector('#isbn').value = '';
     }
 }
+
+// Classe Store: lida com Storage local do navegador web
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+        books.forEach((book, index) => {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// Evento: Mostrar os Livros
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Evento: Adicionar um Livro
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Busca os valores do formulário
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Validação, verifica se os campos estão preenchidos
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert(
+            'Favor preencha todos os campos!',
+            'danger',
+            'fs-4 fw-bold',
+            'center'
+        );
+    } else {
+        // Instância da classe Book
+        const book = new Book(title, author, isbn);
+
+        // Adiciona o Livro na lista
+        UI.addBookToList(book);
+
+        // Adiciona o Livro no LocalStorage do navegador web
+        Store.addBook(book);
+
+        // Mostra mensagem de sucesso
+        UI.showAlert('Livro adicionado', 'success', 'fs-4 fw-bold', 'center');
+
+        // Limpa os campos do formulário
+        UI.clearFields();
+    }
+});
+
+// Evento: Remover um livro
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remover o livro da UI
+    UI.deleteBook(e.target);
+    // Remove o livro do LocalStorage do browser
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+});
